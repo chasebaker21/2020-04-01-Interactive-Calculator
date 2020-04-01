af3d232d17c5b75f9cfee5edc7e94cf521b6d5b2
Chase Baker <chasebaker@Chases-MBP.attlocal.net>
2020-04-01 14:15:58 -0400

    built calculator

---
+++ calculatorApp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ calculatorApp/src/app/home/home.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  currentNumber = '0';
+  firstOperand = null;
+  operator = null;
+  waitForSecondNumber = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // pulls first and second number from calculator then sets currentNumber "display"
+  getNumber(v: string) {
+    console.log(v);
+    if (this.waitForSecondNumber) {
+      this.currentNumber = v;
+      this.waitForSecondNumber = false;
+    } else {
+      this.currentNumber === '0' ? this.currentNumber = v : this.currentNumber += v;
+    }
+  }
+
+  // adds decimal to current number
+  getDecimal() {
+    if (!this.currentNumber.includes('.')) {
+      this.currentNumber += '.';
+    }
+  }
+  // performs the calculation entered using switch statement 
+  doCalculation(op, secondOp) {
+    switch (op) {
+      case '+':
+        return this.firstOperand += secondOp;
+      case '-':
+        return this.firstOperand -= secondOp;
+      case '*':
+        return this.firstOperand *= secondOp;
+      case '/':
+        return this.firstOperand /= secondOp;
+      case '=':
+        return secondOp;
+    }
+  }
+
+  getOperation(op: string) {
+    console.log(op);
+    if (this.firstOperand === null) {
+      this.firstOperand = Number(this.currentNumber);
+    } else if (this.operator) {
+      const result = this.doCalculation(this.operator, Number(this.currentNumber))
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+    console.log(this.firstOperand);
+  }
+
+  // resets all values
+  clear() {
+    this.currentNumber = '0';
+    this.firstOperand = null;
+    this.operator = null;
+    this.waitForSecondNumber = false;
+  }
+
+
+}
